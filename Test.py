311bca249fd3fd0de21fb63ea202110181847a7e
jeevaa <jeevaa.exe@gmail.com>
2024-03-07 21:51:45 +0530

    all

---
+++ Test.py
@@ -0,0 +1,8 @@
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+# chatbot = ChatBot('MyChatBot')
+# trainer = ChatterBotCorpusTrainer(chatbot)
+# trainer.train("chatterbot.corpus.english")
+
+# response = chatbot.get_response("Hello, how are you?")
+# print(response)
